35a07b258270efcf10b522d77ab81504acb24ce2
Jederilson Sousa Luz <Jederilson@gmail.com>
2021-07-22 19:34:36 -0300

    Página de Profile e API
    Adicionando uma página de profile do usuário. Nela é possível ver as informações salvas, alterá-las e remover a conta.
    
    Para isso, também escrevi algumas funções no arquivo de API.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,4 +4,22 @@ const api = axios.create({
     baseURL: "http://localhost:3333"
 });
 
+export async function DeleteUser(token){
+    api.delete("/delete_user", {
+        headers: {
+            'authorization': `Bearer ${token}`
+        }
+    })
+    .then(() => true)
+    .catch(() => false)
+}
+
+export async function UpdateUser(data, token){
+    await api.patch("/update_user", data, {
+        headers: {
+            'authorization': `Bearer ${token}`
+        },
+    })
+}
+
 export default api;
